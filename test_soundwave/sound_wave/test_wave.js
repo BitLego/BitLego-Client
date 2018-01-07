12a9538c6ba8a6aec09037542e311a4275d5a843
KwonJH <hojak99@gmail.com>
2018-01-07 22:54:48 +0900

    sound wave 랑 노래 합치는거

--- test_soundwave/sound_wave/test_wave.js
+++ test_soundwave/sound_wave/test_wave.js
@@ -3,17 +3,16 @@ var tracUrls = [
     'https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3',
     'https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3',
     'https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3',
-    'https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3'
 ];
 
 for(var t in tracUrls) {
-    console.log(t);
     createWavesurfer(tracUrls[t], t);
 }
 
 function createWavesurfer(link, index) {
     waveList.push(Object.create(WaveSurfer));
 
+    // sound wave 설정
     var options = {
         container : document.querySelector('#waveform_'+ index),
         waveColor: '#d32f2f',
@@ -23,8 +22,11 @@ function createWavesurfer(link, index) {
     };
 
     waveList[index].init(options);
+
+    // 트랙 설정
     waveList[index].load(link);
 
+    // sound wave 클릭 시 전체 모두 같이 이동하게
     waveList[index].drawer.on('click', function(event, progress) {
         for(var i =0; i<tracUrls.length; i++) {
             waveList[i].seekTo(progress);
@@ -33,7 +35,8 @@ function createWavesurfer(link, index) {
 }
 
 var slider = document.querySelector('#slider');
-  
+
+// 줌
 slider.oninput = function () {
     var zoomLevel = Number(slider.value);
     for(var i in waveList) {
